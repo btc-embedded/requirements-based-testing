f6089f94b7161283757142ba8a5ec8b59dcddcd8
Thabo Krick <thabokrick@gmail.com>
2025-04-02 12:50:58 +0200

    updated README and pipeline #ci

--- scripts/run_tests.py
+++
@@ -1,8 +0,0 @@
-from btc_tests import run_mil_tests, run_sil_tests
-
-if __name__ == "__main__":
-    success = run_mil_tests()
-    if success:
-        success = run_sil_tests()
-    else:
-        print("MIL tests failed. Skipping SIL tests, coverage, etc.")
