65e90c877fbaa9c0e243a1e5df32408e67f6e45e
Thabo Krick <thabo.krick@btc-embedded.com>
2026-03-09 13:30:22 +0100

    new baseline

--- model/init.m
+++ model/init.m
@@ -1,12 +1,12 @@
-
-TemperatureRanges_TemperatureStage1 = 30;
-TemperatureRanges_TemperatureStage2 = 35;
-TemperatureRanges_TemperatureStage3 = 45;
+TemperatureRanges_LOW = 30;
+TemperatureRanges_MEDIUM = 35;
+TemperatureRanges_HIGH = 45;
 
 RTE_OK = 0;
 
 try
-    seat_heating_control_defineIntEnumTypes
+    Simulink.defineIntEnumType( 'IDT_PowerState', ...
+   {'NOK','OK'}, ...
+   [0 1], ...
+   'AddClassNameToEnumNames', false);
 end
-
-addpath(fileparts(fileparts(mfilename('fullpath'))));
